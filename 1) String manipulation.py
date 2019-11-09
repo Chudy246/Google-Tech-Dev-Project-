a5c6741271f5457547cec00b577ce57bb5834347
Chudy246 <57534920+Chudy246@users.noreply.github.com>
2019-11-09 21:56:30 +0000

    Implemented the code for the first challenge regarding String manipulation

---
+++ 1) String manipulation.py
@@ -0,0 +1,25 @@
+
+S = "abppplee"
+D = {"able", "ale", "apple", "bale", "kangaroo"}
+
+
+def IsIt(Sentence,Word):
+    n = 0
+    Index = 0
+    for i in Word:
+        t = Sentence[Index:].find(i)
+        if t > -1:
+            n += 1
+            Index += t
+    if n == len(Word):
+        return n
+    return 0
+
+def Challenge(Sentence,SetOfWords):
+    n = ""
+    for i in SetOfWords:
+        if IsIt(Sentence,i) > len(n):
+            n = i
+    return n
+
+print(Challenge(S,D))
